d0524b0de34795da76299d5cebfeb53ce4f92836
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-01 10:50:31 -0500

    Update Pais.java
    Added all the body of the Pais class
    
    Co-Authored-By: ElJuanchito <99854835+eljuanchito@users.noreply.github.com>

--- Ejercicio #2 Min Salud/Finished/src/model/Pais.java
+++ Ejercicio #2 Min Salud/Finished/src/model/Pais.java
@@ -1,5 +1,22 @@
 package model;
 
-public class Pais {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Pais {
+	private String nombre;
+	private final List<Departamento> listaDepartamentos = new ArrayList<Departamento>();
+
+	public Pais(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int obtenerCantidadNinos() {
+		int cant = 0;
+		for (int i = 0; i < listaDepartamentos.size(); i++) {
+			Departamento departamento =  listaDepartamentos.get(i);
+			cant += departamento.obtenerCantidadNinos();
+		} return cant;
+	}
+	
 }
